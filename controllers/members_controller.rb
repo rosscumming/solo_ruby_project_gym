df991ed509373fd9a201d7be4bcc8170305ef252
Ross <rosso5792@gmail.com>
2019-12-15 19:10:29 +0000

    members can now be editted and updated

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -3,7 +3,27 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/member.rb' )
 also_reload( '../models/*' )
 
+#members list
 get '/members/?' do
   @members = Member.all()
   erb(:"members/index")
+end
+
+#show
+get '/members/:id' do
+  @member = Member.find(params[:id].to_i)
+  erb(:"members/show")
+end
+
+#edit
+get '/members/:id/edit' do
+  @members = Member.all()
+  @member = Member.find(params[:id])
+  erb(:"members/edit")
+end
+
+#update
+post '/members/:id' do
+ Member.new(params).update()
+  redirect to '/members'
 end
