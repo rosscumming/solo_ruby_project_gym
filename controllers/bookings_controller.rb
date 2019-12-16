013081aaccb3aaeb76550a92eaf5870243cc9165
Ross <rosso5792@gmail.com>
2019-12-16 22:38:29 +0000

    some updates done to order of buttons

---
+++ controllers/bookings_controller.rb
@@ -0,0 +1,11 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative( '../models/booking.rb' )
+require_relative( '../models/session.rb' )
+require_relative( '../models/member.rb' )
+also_reload( '../models/*' )
+
+get '/bookings' do
+  @bookings = Booking.all()
+  erb(:"bookings/index")
+end
